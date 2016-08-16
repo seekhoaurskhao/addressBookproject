7c56075fd7a37e8e75ca2acdf163035062821ed7
Umair <umair.uashah@gmail.com>
2016-08-16 18:56:40 +0200

    working copy of addressbook project

---
+++ addressbookcontroller.cpp
@@ -0,0 +1,6 @@
+#include "addressbookcontroller.h"
+
+AddressBookController::AddressBookController(QObject *parent) : QObject(parent)
+{
+
+}
---
+++ addressbookentry.cpp
@@ -0,0 +1,6 @@
+#include "addressbookentry.h"
+
+AddressBookEntry::AddressBookEntry(QObject *parent) : QObject(parent)
+{
+
+}
